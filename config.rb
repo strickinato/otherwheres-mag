153ddd99f5c81d49710609fe1d44a5444a4115e6
Aaron Strick <aaronstrick@gmail.com>
2016-02-27 17:23:53 -0800

    Remove bitters, add nav bar styling from vendor

--- config.rb
+++ config.rb
@@ -18,6 +18,8 @@ page '/*.txt', layout: false
 
 # General configuration
 
+set :images_dir, "assets"
+
 # Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
