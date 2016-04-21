5b732c86d014ba21bf091aafcff5db5caf0bc8c7
Aaron Strick <aaronstrick@gmail.com>
2016-04-20 18:10:59 -0700

    Site updated to e6179ce

--- javascripts/main/issues/About.elm
+++ javascripts/main/issues/About.elm
@@ -14,22 +14,22 @@ view address model =
     styles =
       style
         [ ( "background-image", "url(assets/issues/about/bg.png)" )
-        , ( "background-size", "cover")
-        , ( "background-position", "bottom")
+        , ( "background-size", "cover" )
+        , ( "background-position", "bottom" )
         , ( "height", "100%" )
         , ( "text-align", "center" )
         , ( "color", "white" )
         ]
   in
     div
-      [ class "issue-content"]
+      [ class "issue-content" ]
       [ div
-        [ styles ]
-        [ viewHeader
-        , viewLiteIs
-        , viewChangingText currentPhrase
-        , viewLine
-        ]
+          [ styles ]
+          [ viewHeader
+          , viewLiteIs
+          , viewChangingText currentPhrase
+          , viewLine
+          ]
       ]
 
 
@@ -38,7 +38,7 @@ viewHeader =
   let
     styles =
       style
-        [ ( "padding-top", "120px" ) ]
+        [ ( "padding-top", "96px" ) ]
   in
     h1 [ styles ] [ text ("OTHERWHERES") ]
 
@@ -48,7 +48,7 @@ viewLiteIs =
   let
     styles =
       style
-        [ ( "padding-top", "36px" )
+        [ ( "padding-top", "20px" )
         , ( "font-size", "26px" )
         ]
   in
@@ -60,12 +60,13 @@ viewChangingText currentText =
   let
     styles =
       style
-        [ ( "padding-top", "36px" )
+        [ ( "padding-top", "20px" )
         , ( "font-size", "26px" )
         , ( "padding-bottom", "8px" )
         ]
   in
     h3 [ styles ] [ text (String.toUpper currentText) ]
+
 
 viewLine : Html
 viewLine =
@@ -73,13 +74,13 @@ viewLine =
     styles =
       style
         [ ( "border", "1px solid " ++ grey )
-        , ( "height", "0px")
-        , ( "width", "80%")
-        , ( "margin", "0 auto")
+        , ( "height", "0px" )
+        , ( "width", "80%" )
+        , ( "margin", "0 auto" )
         ]
   in
     div [ styles ] []
-  
+
 
 grey : String
 grey =
