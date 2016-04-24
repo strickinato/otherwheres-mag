18c791e3ce20a7accbc15953d57532cb97316d7d
Aaron Strick <aaronstrick@gmail.com>
2016-04-23 17:52:24 -0700

    Fix some mobile styling

--- source/javascripts/main/Mobile/View.elm
+++ source/javascripts/main/Mobile/View.elm
@@ -13,7 +13,10 @@ view =
       [ p [] [ text "{ mostly } true" ]
       , p [] [ text "stories" ]
       ]
-    , p [] [ text "This screen is hella small" ] 
-    , p [] [ text "We're currently working hard on a version of our site that works well on screens like this one. " ]
-    , p [] [ text "Until then, check us out on your computer (or make your browser bigger)." ]
+    , div
+        [ class "mobile-explanatory-text"]
+        [ p [] [ text "This screen is hella small" ] 
+        , p [] [ text "We're currently working hard on a version of our site that works well on screens like this one. " ]
+        , p [] [ text "Until then, check us out on your computer (or make your browser bigger)." ]
+        ]
     ]
